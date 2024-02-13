4043a553a6332770ddbe6ad2d3afaa1a3b630b04
WTL04 <114207423+WTL04@users.noreply.github.com>
2024-02-13 11:58:13 -0800

    Added comments to war.cpp

--- war.cpp
+++ war.cpp
@@ -1,3 +1,9 @@
+// William Luu
+// Class (CECS 325-02)
+// Program 1 - War
+// 2/15/2024
+//I certify that this program is my own original work. I did not copy any part of this program from any other source. I further certify that I typed each and every line of code in this program.
+
 #include <iostream>
 #include "card.h"
 #include "deck.h"
